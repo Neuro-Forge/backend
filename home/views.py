87edee81c459bea6cacb204e752e1769e6b6493f
cozmek ai <cozmekaifacilitatora@gmail.com>
2026-04-02 12:17:10 +0530

    database updated

--- home/views.py
+++ home/views.py
@@ -7,30 +7,31 @@ def home(request):
     return render(request, 'home.html')
 
 def qoutes(request):
+    if request.method == 'POST':
+        submitted_text = request.POST.get('qoute')
+        if submitted_text:
+            Quote.objects.create(quote=submitted_text)
+
+    quote_list = Quote.objects.all()
+    selected_quote = random.choice(quote_list) if quote_list else None
+    selected_quote_text = selected_quote.quote if selected_quote else None
+
+    request.session['quote'] = selected_quote_text
     
-    quote_options = [
+    
+    defult_quotes = [
         "The only way to do great work is to love what you do. - Steve Jobs",
-        "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer",
+        "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
         "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
         "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-        "Believe you can and you're halfway there. - Theodore Roosevelt",
-        "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-        "The best way to predict the future is to create it. - Peter Drucker",
+        "Believe you can and you're halfway there. - Theodore Roosevelt",  
+        "The only way to do great work is to love what you do. - Steve Jobs",
+        "do not let the fear of losing be greater than the excitement of winning. - Robert Kiyosaki",
     ]
-    selected_quote = random.choice(quote_options)
-    submitted_qoute = None
-    if request.method == 'POST':
-        submitted_qoute = request.POST.get('qoute')
-        if submitted_qoute:
-            quote_options.append(submitted_qoute)
-            Quote.objects.create(quote=submitted_qoute)
-    
-    qoutes_list = Quote.objects.all()
-    data = {
-        'qoute': selected_quote,
-        'all_qoutes': quote_options,
-        'submitted_qoute': submitted_qoute,
-        'qoutes_list': qoutes_list,
-    }
-    
-    return render(request, 'qoutes.html',data)
+
+    random_quote = random.choice(defult_quotes)
+    return render(request, 'qoutes.html', {
+        'submitted_qoute': selected_quote_text,
+        'quote_list': quote_list,
+        'random_quote': random_quote,
+    })
